5d6ed86b80c1b7afc59f61fc6bb70843b29245d6
Lul-Abdifan <tasanbannu@gmail.com>
2023-09-24 02:17:32 -0400

    Add client side vaidation

--- src/components/Contact/ContactMe.jsx
+++ src/components/Contact/ContactMe.jsx
@@ -2,23 +2,58 @@
 import { Container } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
-
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 
 const ContactMe = () => {
+  const [data,setData]=useState({
+    message:'',
+    from_name:'',
+    from_email:''
+  })
     const form = useRef();
     const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
     const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
      const apiKey = process.env.REACT_APP_EMAILJS_API_KEY;
+ const handleChange =(event)=>{
+  const {name,value} = event.target
+  console.log(name,value);
+ setData({...data,[name]:value})
+ }
+
   const handleSubmit =(event)=>{
     event.preventDefault();
+    setData({
+      message:'',
+      from_name:'',
+      from_email:''
+    })
     emailjs.sendForm(serviceId, templateId, form.current, apiKey)
     .then((result) => {
-        console.log(result.text);
+      console.log(result);
+      toast.success("Email sent successfully!", {
+        position: "top-right",
+        autoClose: 3000, 
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+      })
+     
+    
+
+
     }, (error) => {
         console.log(error.text);
+        toast.error("Oop,Email is not sent!", {
+          position: "top-right",
+          autoClose: 3000, 
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+        })
     });
 };
   
@@ -33,17 +68,17 @@ const ContactMe = () => {
 <div className="d-flex justify-content-center">
 <Form className="m-3" ref={form} onSubmit={handleSubmit}>
  <Form.Group className="m-3" controlId="exampleForm.ControlInput1">
-        <Form.Control type="text" name="from_name" placeholder="Full Name" className="custom-input"/>
+        <Form.Control type="text" name="from_name" placeholder="Full Name" className="custom-input" value={data.from_name} onChange={handleChange}/>
       </Form.Group>
       <Form.Group className="m-3" controlId="exampleForm.ControlInput1">
-        <Form.Control type="email" name="from_email" placeholder="Email" className="custom-input"/>
+        <Form.Control type="email" name="from_email" placeholder="Email" required className="custom-input" value={data.from_email} onChange={handleChange}/>
       </Form.Group>
      
 
       <Form.Group className="m-3 custom-input" controlId="exampleForm.ControlTextarea1">
-       <Form.Control name="message" as="textarea" rows={3} placeholder="I value your message or connection"/>
+       <Form.Control name="message" as="textarea" rows={3} required value={data.message} placeholder="I value your message or connection" onChange={handleChange}/>
       </Form.Group>
-         <Button type="submit" className="m-5" variant="outline-primary">Get In Touch</Button>{' '}
+         <Button type="submit" className="m-5" variant="outline-primary" required>Get In Touch</Button>{' '}
     </Form>
 </div>
         </Container>
